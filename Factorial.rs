d4abb5bfd8a66f2143eea3f55488c3df9922e932
Dario Marti <dario.marti@fadendaten.ch>
2023-01-15 22:17:29 +0100

    Factorial in RUUUUUUUUUST

---
+++ Factorial.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let n = 9;
+    let res = factorial(n);
+
+    println!("{}", res);
+}
+
+fn factorial(a: u32) -> u32 {
+    if a == 1 {
+        return 1;
+    } else {
+        return factorial(a-1) * a;
+    }
+}
