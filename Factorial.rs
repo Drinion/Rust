0aabd8d4811a94d954435fd9759c600e1cb0a073
Dario Marti <dario.marti@fadendaten.ch>
2023-03-12 17:53:09 +0100

    Add comment

--- Factorial.rs
+++ Factorial.rs
@@ -5,14 +5,12 @@ fn main() {
     println!("{}", res);
 }
 
+// SSH test
+
 fn factorial(a: u32) -> u32 {
     if a == 1 {
         return 1;
     } else {
         return factorial(a-1) * a;
     }
-}
-
-fn whatTheFuck() {
-    return 0;
 }
